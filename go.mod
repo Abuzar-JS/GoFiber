dcd3db0560e80b0c452e749e6f127e4c91614fa6
Abuzar <mabuzar533@gmail.com>
2024-07-22 15:52:04 +0500

    Updates

---
+++ go.mod
@@ -0,0 +1,26 @@
+module test
+
+go 1.22.5
+
+require (
+	github.com/gofiber/fiber v1.14.6
+	github.com/joho/godotenv v1.5.1
+	golang.org/x/mod v0.19.0
+	gorm.io/gorm v1.25.11
+)
+
+require (
+	github.com/andybalholm/brotli v1.1.0 // indirect
+	github.com/gofiber/utils v0.0.10 // indirect
+	github.com/gorilla/schema v1.1.0 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/klauspost/compress v1.17.9 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasthttp v1.55.0 // indirect
+	github.com/valyala/tcplisten v1.0.0 // indirect
+	golang.org/x/sys v0.22.0 // indirect
+	golang.org/x/text v0.16.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,1 +1,53 @@
 package main
+
+import (
+	"log"
+
+	"github.com/gofiber/fiber"
+	"github.com/joho/godotenv"
+	"golang.org/x/mod/sumdb/storage"
+	"gorm.io/gorm"
+)
+
+type Book struct {
+	Id        int    `json:"id"`
+	Author    string `json:"author"`
+	Title     string `json:"title"`
+	Publisher string `json:"publisher"`
+}
+
+type Repository struct {
+	DB *gorm.DB
+}
+
+// func Setup Routes
+func (r *Repository) SetupRoutes(app *fiber.App) {
+	api := app.Group("/api")
+	api.Post("/create_books", r.CreateBook)
+	api.Delete("delete_book/id", r.deleteBook)
+	api.Get("/get_books", r.GetBooks)
+	api.Get("/books", r.GetBooks)
+
+}
+
+func main() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	db, err := storage.NewConnection(config)
+
+	if err != nil {
+		log.Fatal("could not load the data")
+	}
+
+	r := Repository{
+		DB: db,
+	}
+
+	app := fiber.New()
+	r.SetupRoutes(app)
+	app.Listen(":8080")
+
+}
